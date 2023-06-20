e6499e7ba874179a16f041d33e0eb74d4738f1b8
devnishant29 <devnishant29@gmail.com>
2023-06-20 16:00:24 +0530

    mongoose complete

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+    'mongodb+srv://devnishant29:mybirthdateis30@n.nxixkin.mongodb.net/N?retryWrites=true&w=majority'
   )
   .then(result => {
     User.findOne().then(user => {
